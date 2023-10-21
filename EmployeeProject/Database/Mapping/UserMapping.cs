208242047fb30d3f26059942c2b2272ed14897e1
vishnu singh <vishnu.pratap88singh@gmail.com>
2023-10-21 23:21:59 +0530

    Add class To Map users model with database

---
+++ EmployeeProject/Database/Mapping/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿using Employee.Shared.Mappings.Base;
+using Employee.Shared.Model;
+
+namespace EmployeeProject.Database.Mapping
+{
+    public class UserMapping:BsonClassMapping<Users>
+    {
+        public UserMapping() {
+            AutoMap();
+            SetIgnoreExtraElements(true);
+            UnmapProperty(e => e.Id);
+            MapProperty(e=>e.Id).SetElementName("Id");
+        }
+    }
+}
