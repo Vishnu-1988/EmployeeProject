6b59b963f530342a62af876009b5dbc5dfe52569
vishnu singh <vishnu.pratap88singh@gmail.com>
2023-10-21 22:27:02 +0530

    Shared Modules created to call mongo service and Mongo client

---
+++ Employee.Infrastructure/SharedModule.cs
@@ -0,0 +1,37 @@
+﻿using Employee.Infrastructure.Service;
+using Employee.Infrastructure.Service.Interface;
+using Employee.Shared.MongoDb;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Security.Authentication;
+
+namespace Employee.Infrastructure
+{
+    public static class SharedModule
+    {
+        public static IServiceCollection AddSharedModule(this IServiceCollection services)
+        {
+            services.TryAddSingleton(e =>
+            {
+                var cosmosDbOptions = e.GetService<IOptions<DatabaseSettings>>().Value;
+                return GetMongoClient(cosmosDbOptions.ConnectionString);
+            }); 
+            services.TryAddScoped<IMongoCollectionService,MongoCollectionService>();
+            return services;
+        }
+
+        private static IMongoClient GetMongoClient(string connectionString)
+        {
+            var settings=MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings
+            {
+               
+                    EnabledSslProtocols = SslProtocols.Tls12
+                
+            };
+            return new MongoClient(settings);
+        }
+    }
+}
