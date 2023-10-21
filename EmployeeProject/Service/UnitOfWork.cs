3423db0998de0caef6d35fcc21e8dde7ac5d25b7
vishnu singh <vishnu.pratap88singh@gmail.com>
2023-10-21 23:24:54 +0530

    Created interface and class  to provide services to controller to perform database i.e mongo db call using UnitOfWork

---
+++ EmployeeProject/Service/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Employee.Shared.Model;
+using EmployeeProject.Service.Interface;
+
+namespace EmployeeProject.Service
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public int GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Users> GetUsers(string Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
